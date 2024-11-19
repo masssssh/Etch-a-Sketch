5ef472bd3c147db1b3052eb6103cadbcdfbf2911
masssssh <pykpykpyk000@yandex.ru>
2024-11-19 21:03:48 +0300

    mouseenter and mousemove 's effect

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ for (let i=1; i<=16; i++) {
     div.appendChild(element);
 }
 
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const a = 0.3;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+
+const elements = div.querySelectorAll('p');
+
+elements.forEach(p => {
+    p.addEventListener('mouseenter', () => {
+    p.style.backgroundColor = randomColor();    
+    });
+});
+
+elements.forEach(p => {
+    p.addEventListener('mouseover', () => {
+    p.style.backgroundColor = randomColor();    
+    });
+});
+
