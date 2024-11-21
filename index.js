cbf142c65402c6c925eb8766ed14a99a54ec64ac
masssssh <pykpykpyk000@yandex.ru>
2024-11-22 00:22:00 +0300

    add button

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const div = document.querySelector('div');
-
-
-for (let i=1; i<=16; i++) {
-    const element = document.createElement('p');
-    div.appendChild(element);
-}
+const btn = document.querySelector('.btn');
 
 function randomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    const a = 0.3;
+    const a = 0.5;
     return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
 
+btn.addEventListener('click', () => {
+    let newGrid = prompt('Введите количество ячеек:');
+    if (newGrid <= 0 || isNaN(newGrid)) {
+        alert ('Введите корректное число больше 0!');
+    return;
+    } else if (newGrid >= 100) {
+        alert ('Введите число меньше 100');
+        return;
+}
+    div.innerHTML = '';
+    
+    const cellSize = Math.floor (600 / newGrid);
 
-const elements = div.querySelectorAll('p');
+    for (let i = 1; i <= newGrid * newGrid; i++) {
+        const element = document.createElement('p');
+        element.style.width = `${cellSize}px`;
+        element.style.height = `${cellSize}px`;
+        element.style.boxSizing = 'border-box';
+        div.appendChild(element);
+    
+    element.addEventListener('mouseenter', () => {
+        element.style.backgroundColor = randomColor();    
+        });
+    }
 
-elements.forEach(p => {
-    p.addEventListener('mouseenter', () => {
-    p.style.backgroundColor = randomColor();    
-    });
 });
-
-elements.forEach(p => {
-    p.addEventListener('mouseover', () => {
-    p.style.backgroundColor = randomColor();    
-    });
-});
-
